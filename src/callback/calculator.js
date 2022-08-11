9b0ce6189352be31a84cebc02c300611dfe95243
Arias <miguelariasgiraldo2000@gmail.com>
2022-08-11 11:16:02 -0500

    Callback calculator created

---
+++ src/callback/calculator.js
@@ -0,0 +1,19 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function rest(num1, num2) {
+    return num1 - num2;
+}
+
+function mult(num1, num2) {
+    return num1 * num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2);
+};
