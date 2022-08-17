e40eff8cbef09768e64b1f4ad480331e3bddd473
Arias <miguelariasgiraldo2000@gmail.com>
2022-08-17 18:04:17 -0500

    Creating callback hell as a bad practice example.

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,10 +1,10 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;;
-
+const apiUrl = 'https://api.escuelajs.co/api/v1';
 
 function callApi(urlApi, callback)
 {
     let xhttp = new XMLHttpRequest();
-    xhttp.open('GET', urlApi, true); //Abrir la conexión
+    xhttp.open('GET', urlApi, true); //Open connection
     xhttp.onreadystatechange = function(event)
     {
         if(xhttp.readyState === 4)
@@ -12,13 +12,19 @@ function callApi(urlApi, callback)
             if (xhttp.status === 200) 
             {
                 callback(JSON.parse(xhttp.responseText));
+            }else{
+                callback("Error");
             }
-        }
-        else{
-            callback("Error");
         }
     }
     xhttp.send();
 }
 
-callApi("https://api.escuelajs.co/api/v1/products", console.log)
+callApi(`${apiUrl}/products`, (error1, data1)=>{
+    if(error1) return console.log(error1);
+    callApi(`${apiUrl}/products/${data1[0]?.id}`, (error2, data2)=>{
+        if(error2) return console.log(error2);
+        console.log(data1);
+        console.log(data2);
+    })
+})
