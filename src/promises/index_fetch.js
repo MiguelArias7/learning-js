76ac4fb5594c0256e49faf49dc57d89bcdd9e80c
Arias <miguelariasgiraldo2000@gmail.com>
2022-08-23 14:22:08 -0500

    Example with fetch and promises.

--- src/promises/index_fetch.js
+++ src/promises/index_fetch.js
@@ -1,12 +1,21 @@
-import fetch from "node-fetch";
-const apiUrl = "https://api.escuelajs.co/api/v1";
+import fetch from 'node-fetch';
+const apiUrl = 'https://api.escuelajs.co/api/v1';
 
-function fetchData(urlApi)
-{
-    return fetch(urlApi)
+function fetchData(urlApi) {
+  return fetch(urlApi);
 }
 
+// fetchData(`${apiUrl}/products`)
+//   .then((response) => response.json())
+//   .then((products) => console.log(products))
+//   .catch((error) => console.log(error));
+
 fetchData(`${apiUrl}/products`)
-.then(response => response.json())
-.then(products => console.log(products))
-.catch(error => console.log(error))
+  .then((response) => response.json())
+  .then((products) => {
+    // console.log(products);
+    return fetchData(`${apiUrl}/products/${products[0].id}`);
+  })
+  .then((response) => response.json())
+  .then((product) => console.log(product))
+  .catch((error) => console.log(error));
