717d3dea5cbbdf880cab2ae5e30a607ca5595ff9
Arias <miguelariasgiraldo2000@gmail.com>
2022-08-24 11:31:26 -0500

    Example with generator, async functions and fetch

---
+++ src/generator/index.js
@@ -0,0 +1,20 @@
+function* gen() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+const g = gen();
+console.log(g.next().value);
+console.log(g.next().value);
+
+function* iterate(array) {
+  for (const value of array) {
+    yield value;
+  }
+}
+
+const it = iterate(['Oscar', 'Miguel', 'Sebastian']);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
